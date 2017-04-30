515915dc3b54bfba9ffddfb41a18abed5a91b28f
cexoso <285516193@qq.com>
2017-04-30 13:57:20 +0800

    加入decorator特性

--- app/js/app.tsx
+++ app/js/app.tsx
@@ -1,6 +1,23 @@
-import React from "react"
-export default function App() {
-    return (<div>
+import * as React from "react";
+import {Component} from "react";
+function log(target, key: string, value: any) {
+    console.log(value)
+    return function() {
+        console.log(22)
+        return 2
+    }
+}
+export default class App extends Component {
+    initTime = new Date()
+    @log
+    click() {
+        console.log(1)
+        // console.log(this.initTime)
+    }
+    render() {
+        return (<div onClick={this.click}>
         13
     </div>)
+    }
 }
+
